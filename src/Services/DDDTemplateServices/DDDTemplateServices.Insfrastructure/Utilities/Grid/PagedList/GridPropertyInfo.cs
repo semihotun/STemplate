8fe262da5fccb2b838bab0ae579a219510759b34
semih <semihotun1@gmail.com>
2023-12-26 18:51:29 +0300

    Add Dynamic Grid Expression

---
+++ src/Services/DDDTemplateServices/DDDTemplateServices.Insfrastructure/Utilities/Grid/PagedList/GridPropertyInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace DDDTemplateServices.Insfrastructure.Utilities.Grid.PagedList
+{
+    /// <summary>
+    /// Grid Property
+    /// </summary>
+    public class GridPropertyInfo
+    {
+        public string? PropertyName { get; set; }
+        public string? PropertyType { get; set; }
+    }
+}
---
+++ src/Services/DDDTemplateServices/DDDTemplateServices.Insfrastructure/Utilities/Grid/PagedList/PagedList.cs
@@ -0,0 +1,35 @@
+﻿namespace DDDTemplateServices.Insfrastructure.Utilities.Grid.PagedList
+{
+    /// <summary>
+    /// paged list for grid 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class PagedList<T> : IPagedList<T>
+    {
+        public List<T> Data { get; set; }
+        public int PageIndex { get; set; }
+        public int PageSize { get; set; }
+        public int TotalCount { get; set; }
+        public int TotalPages { get; set; }
+        public bool HasPreviousPage => PageIndex > 0;
+        public bool HasNextPage => PageIndex + 1 < TotalPages;
+        public IEnumerable<GridPropertyInfo> PropertyInfos { get; set; }
+        public PagedList()
+        {
+            Data = new List<T>();
+            PageIndex = 0;
+            PageSize = 0;
+            TotalCount = 0;
+            PropertyInfos = new List<GridPropertyInfo>();
+        }
+        public PagedList(List<T> data, int pageIndex, int pageSize, int totalCount, int totalPages, IEnumerable<GridPropertyInfo> propertyInfos)
+        {
+            Data = data;
+            PageIndex = pageIndex;
+            PageSize = pageSize;
+            TotalCount = totalCount;
+            TotalPages = totalPages;
+            PropertyInfos = propertyInfos;
+        }
+    }
+}
