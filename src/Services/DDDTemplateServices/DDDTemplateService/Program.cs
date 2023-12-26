198dc15d58cc6db16bb14105951e7b0a75d335f3
semih <semihotun1@gmail.com>
2023-12-26 18:34:05 +0300

    Add Project configure json

---
+++ src/Services/DDDTemplateServices/DDDTemplateService/Program.cs
@@ -0,0 +1,55 @@
+using DDDTemplateService.Application.Assemblies;
+using DDDTemplateService.Extensions;
+using DDDTemplateServices.Insfrastructure.Utilities.Caching.Redis;
+using DDDTemplateServices.Insfrastructure.Utilities.Cors;
+using DDDTemplateServices.Insfrastructure.Utilities.Exceptions.GlobalEror;
+using DDDTemplateServices.Insfrastructure.Utilities.Ioc;
+using DDDTemplateServices.Insfrastructure.Utilities.Logging;
+using DDDTemplateServices.Insfrastructure.Utilities.MediatR;
+using DDDTemplateServices.Insfrastructure.Utilities.ServiceBus;
+using DDDTemplateServices.Persistence.Extensions;
+using DDDTemplateServices.Persistence.GenericRepository;
+using FluentValidation;
+using Prometheus;
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddHttpContextAccessor();
+builder.AddCors();
+builder.Services.AddMvc();
+//Log-Cache-Mediatr-FluentValidation-Mass transit
+builder.AddSerilog();
+builder.AddRedis();
+var assembly = ApiAssemblyExtensions.GetLibrariesAssemblies();
+builder.Services.ConfigureDbContext(builder.Configuration);
+builder.AddMediatR(assembly);
+builder.Services.AddValidatorsFromAssembly(ApplicationAssemblyExtension.GetApplicationAssembly(), includeInternalTypes: true);
+builder.AddCustomMassTransit(assembly, (busRegistrationContext, busFactoryConfigurator) =>
+{
+    busFactoryConfigurator.AddPublishers();
+});
+//Service Registered
+builder.Services.AddTransient<IMailService, MailManager>();
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
+//Build
+var app = builder.Build();
+//WebUI
+app.UseSwagger();
+app.UseSwaggerUI();
+if (!app.Environment.IsDevelopment())
+{
+    //Metrics
+    app.UseHttpMetrics();
+    app.MapMetrics();
+    app.UseHttpsRedirection();
+}
+//Core
+app.UseCors();
+app.UseStaticFiles();
+app.UseAuthorization();
+app.MapControllers();
+//MiddleWare
+app.UseMiddleware<ExceptionMiddleware>();
+ServiceTool.ServiceProvider = app.Services;
+app.Run();
