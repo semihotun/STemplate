f6e75fe59aca1b8a75672c776a89f2b7dcd5a718
semih <semihotun1@gmail.com>
2023-12-26 18:56:55 +0300

    Add Mediatr

---
+++ src/Services/DDDTemplateServices/DDDTemplateServices.Insfrastructure/Utilities/MediatR/MediatRExtension.cs
@@ -0,0 +1,27 @@
+﻿using DDDTemplateServices.Insfrastructure.Utilities.MediatorBehaviour.Performance;
+using DDDTemplateServices.Insfrastructure.Utilities.MediatorBehaviour.Validation;
+using MediatR;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+namespace DDDTemplateServices.Insfrastructure.Utilities.MediatR
+{
+    /// <summary>
+    /// mediatr add to webbapp
+    /// </summary>
+    public static class MediatRExtension
+    {
+        public static WebApplicationBuilder AddMediatR(this WebApplicationBuilder builder,
+            Assembly[] assembly, Action<WebApplicationBuilder>? builderAction = null)
+        {
+            builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+            builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(PerformanceBehavior<,>));
+            builderAction?.Invoke(builder);
+            builder.Services.AddMediatR(cfg =>
+            {
+                cfg.RegisterServicesFromAssemblies(assembly);
+            });
+            return builder;
+        }
+    }
+}
