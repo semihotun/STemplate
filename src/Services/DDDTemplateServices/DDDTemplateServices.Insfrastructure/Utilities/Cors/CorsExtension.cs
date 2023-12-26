518c241ff0c547cb7d1eb0d0f21abd5f47831996
semih <semihotun1@gmail.com>
2023-12-26 18:54:55 +0300

    Add Cors

---
+++ src/Services/DDDTemplateServices/DDDTemplateServices.Insfrastructure/Utilities/Cors/CorsExtension.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+namespace DDDTemplateServices.Insfrastructure.Utilities.Cors
+{
+    /// <summary>
+    /// appgateway and service port added to cors
+    /// </summary>
+    public static class CorsExtension
+    {
+        public static WebApplicationBuilder AddCors(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddCors(options =>
+            options.AddDefaultPolicy(policy => policy
+              .WithOrigins("https://localhost:5001",
+              "https://localhost:5000",
+              "http://localhost:5001",
+              "http://localhost:5000")
+              .AllowAnyHeader()
+              .AllowAnyMethod()
+              .AllowCredentials()));
+            return builder;
+        }
+    }
+}
