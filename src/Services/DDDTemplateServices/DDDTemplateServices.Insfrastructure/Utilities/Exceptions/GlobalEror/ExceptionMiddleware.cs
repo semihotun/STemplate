bbb34cbaf4aab94f981dce59d75f39329f146db4
semih <semihotun1@gmail.com>
2023-12-26 18:58:06 +0300

    Add Global Error

---
+++ src/Services/DDDTemplateServices/DDDTemplateServices.Insfrastructure/Utilities/Exceptions/GlobalEror/ExceptionMiddleware.cs
@@ -0,0 +1,77 @@
+﻿using DDDTemplateServices.Insfrastructure.Utilities.MediatorBehaviour.Validation.Exceptions;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using Serilog;
+using System.Net;
+using System.Security;
+namespace DDDTemplateServices.Insfrastructure.Utilities.Exceptions.GlobalEror
+{
+    /// <summary>
+    /// all eror arrived middleware
+    /// </summary>
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception e)
+            {
+                await HandleExceptionAsync(httpContext, e);
+            }
+        }
+        private async Task HandleExceptionAsync(HttpContext httpContext, Exception e)
+        {
+            var errorDetails = new ErrorDetails();
+            errorDetails.StatusCode = (int)HttpStatusCode.InternalServerError;
+            httpContext.Response.ContentType = "application/json";
+            httpContext.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            if (e.GetType() == typeof(CustomValidatonException))
+            {
+                var validationErorDetail = new ValidationErorDetail();
+                validationErorDetail.Message = JsonConvert.DeserializeObject<IEnumerable<ValidationData>>(e.Message);
+                validationErorDetail.StatusCode = (int)HttpStatusCode.BadRequest;
+                validationErorDetail.ErrorType = "ValidationException";
+                var customValidationObject = JsonConvert.SerializeObject(validationErorDetail);
+                Log.Error(customValidationObject);
+                await httpContext.Response.WriteAsync(customValidationObject);
+                return;
+            }
+            else if (e.GetType() == typeof(UnauthorizedAccessException))
+            {
+                errorDetails.Message = e.Message;
+                errorDetails.StatusCode = StatusCodes.Status401Unauthorized;
+                errorDetails.ErrorType = "UnauthorizedAccessException";
+                httpContext.Response.StatusCode = StatusCodes.Status401Unauthorized;
+            }
+            else if (e.GetType() == typeof(SecurityException))
+            {
+                errorDetails.Message = e.Message;
+                errorDetails.StatusCode = StatusCodes.Status401Unauthorized;
+                errorDetails.ErrorType = "SecurityException";
+                httpContext.Response.StatusCode = StatusCodes.Status401Unauthorized;
+            }
+            else if (e.GetType() == typeof(ApplicationException))
+            {
+                errorDetails.Message = e.Message;
+                errorDetails.StatusCode = (int)HttpStatusCode.BadRequest;
+                errorDetails.ErrorType = "ApplicationException";
+                httpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+            }
+            else
+            {
+                errorDetails.Message = "Unexpected error";
+            }
+            var serializeObject = JsonConvert.SerializeObject(errorDetails);
+            Log.Error(serializeObject);
+            await httpContext.Response.WriteAsync(serializeObject);
+        }
+    }
+}
