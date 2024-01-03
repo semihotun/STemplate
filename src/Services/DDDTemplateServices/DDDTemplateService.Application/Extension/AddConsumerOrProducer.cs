96ec87a4f731ad76d53af61e8e0c48c902a5d8c6
semih <semihotun1@gmail.com>
2024-01-03 03:23:45 +0300

    Move to files addconsumerorproducer
    Move to files addconsumerorproducer

---
+++ src/Services/DDDTemplateServices/DDDTemplateService.Application/Extension/AddConsumerOrProducer.cs
@@ -0,0 +1,25 @@
+﻿using DDDTemplateServices.Insfrastructure.Utilities.AdminRole;
+using DDDTemplateServices.Insfrastructure.Utilities.ServiceBus;
+using MassTransit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDDTemplateService.Application.Extension
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class AddConsumerOrProducer
+    {
+        public static void AddPublishers(this IRabbitMqBusFactoryConfigurator cfg)
+        {
+            cfg.AddDirectProducer<AddAdminRoleIntegrationEvent>();
+        }
+        public static void AddConsumers(this IRabbitMqBusFactoryConfigurator cfg, IBusRegistrationContext ctx)
+        {
+        }
+    }
+}
