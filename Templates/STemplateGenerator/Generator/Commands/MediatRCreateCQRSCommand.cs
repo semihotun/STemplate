ffc554c345de2f663503145824a4e50b7395f567
semih <semihotun1@gmail.com>
2024-02-01 13:32:00 +0300

    Create all command and query

---
+++ Templates/STemplateGenerator/Generator/Commands/MediatRCreateCQRSCommand.cs
@@ -0,0 +1,21 @@
+﻿namespace Generator.Commands;
+/// <summary>
+/// Generator menü create command and query
+/// </summary>
+[Command(PackageIds.MediatRCreateCQRSCommand)]
+internal sealed class MediatRCreateCQRSCommand : BaseCommand<MediatRCreateCQRSCommand>
+{
+    /// <summary>
+    /// Execute Command
+    /// </summary>
+    /// <param name="e"></param>
+    /// <returns></returns>
+    protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+    {
+        await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+        await VS.Commands.ExecuteAsync(PackageGuids.Generator, PackageIds.MediatRCreateAddHandlerCommand, null);
+        await VS.Commands.ExecuteAsync(PackageGuids.Generator, PackageIds.MediatRCreateDeleteHandlerCommand, null);
+        await VS.Commands.ExecuteAsync(PackageGuids.Generator, PackageIds.MediatRCreateUpdateHandlerCommand, null);
+        await VS.Commands.ExecuteAsync(PackageGuids.Generator, PackageIds.MediatRCreateGetByIdHandlerCommand, null);
+    }
+}
