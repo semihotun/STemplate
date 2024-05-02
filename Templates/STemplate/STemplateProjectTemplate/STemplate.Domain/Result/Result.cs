5a2d643384eff80155dd79e40d4c77c74513f831
semih <semihotun1@gmail.com>
2024-05-02 21:46:41 +0300

    Change Result to static resutl

--- Templates/STemplate/STemplateProjectTemplate/STemplate.Domain/Result/Result.cs
+++ Templates/STemplate/STemplateProjectTemplate/STemplate.Domain/Result/Result.cs
@@ -1,22 +1,37 @@
 ﻿using Newtonsoft.Json;
+/// <summary>
+/// Ardalis Result
+/// </summary>
 namespace STemplate.Domain.Result
 {
-    /// <summary>
-    /// global result classes
-    /// </summary>
-    public class Result
+    public class Result<T>
     {
         [JsonConstructor]
-        public Result(bool success, string message)
-            : this(success)
+        public Result(T? data, bool success, string message)
         {
+            Data = data;
+            Success = success;
             Message = message;
+        }
+        public Result(T? data, bool success)
+        {
+            Data = data;
+            Success = success;
         }
         public Result(bool success)
         {
             Success = success;
         }
-        public bool Success { get; }
-        public string? Message { get; }
+        public Result(bool success, string message)
+        {
+            Success = success;
+            Message = message;
+        }
+        protected Result()
+        {
+        }
+        public T? Data { get; }
+        public bool Success { get; protected set; }
+        public string Message { get; protected set; }
     }
 }
---
+++ Templates/STemplate/STemplateProjectTemplate/STemplate.Domain/Result/ResultVoid.cs
@@ -0,0 +1,32 @@
+﻿namespace STemplate.Domain.Result
+{
+    /// <summary>
+    /// Ardalis Result
+    /// </summary>
+    public class Result : Result<Result>
+    {
+        public static Result SuccessResult(string message)
+        {
+            return new Result() { Message = message };
+        }
+        public static Result ErrorResult(string message)
+        {
+            return new Result() { Message = message, Success = false };
+        }
+        public static Result SuccessResult()
+        {
+            return new Result() { Success = true };
+        }
+        public static Result ErrorResult()
+        {
+            return new Result() { Success = false };
+        }
+        public static Result<T> SuccessDataResult<T>(T value, string successMessage) => new(value, true, successMessage);
+        public static Result<T> SuccessDataResult<T>(T value) => new(value, true);
+        public static Result<T> SuccessDataResult<T>() => new(true);
+        public static Result<T> ErrorDataResult<T>(T value, string successMessage) => new(value, false, successMessage);
+        public static Result<T> ErrorDataResult<T>(T value) => new(value, false);
+        public static Result<T> ErrorDataResult<T>() => new(false);
+        public static Result<T> ErrorDataResult<T>(string message) => new(false, message);
+    }
+}
