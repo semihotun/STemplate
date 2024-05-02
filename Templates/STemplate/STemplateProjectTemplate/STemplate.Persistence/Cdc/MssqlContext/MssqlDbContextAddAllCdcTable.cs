3761d8b66cb96c64fcd988fb4ff2fd753b037199
semih <semihotun1@gmail.com>
2024-05-02 21:45:45 +0300

    Cdc big data problem

--- Templates/STemplate/STemplateProjectTemplate/STemplate.Persistence/Cdc/MssqlContext/MssqlDbContextAddAllCdcTable.cs
+++ Templates/STemplate/STemplateProjectTemplate/STemplate.Persistence/Cdc/MssqlContext/MssqlDbContextAddAllCdcTable.cs
@@ -7,5 +7,6 @@ public static class MssqlDbContextAddAllCdcTable
     public static void AddAllCdc(CoreDbContext ctx)
     {
         ctx.Database.ExecuteSqlRaw("EXEC sys.sp_cdc_enable_table @source_schema = N'dbo', @source_name = N'Outbox', @role_name = NULL;");
+        ctx.Database.ExecuteSqlRaw("EXEC sp_configure 'show advanced options', 1; RECONFIGURE; EXEC sp_configure 'max text repl size', -1; RECONFIGURE;");
     }
 }
