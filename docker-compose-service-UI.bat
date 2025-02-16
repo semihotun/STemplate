96239cab5f94e5e5a9c6553445adbf53b690ad41
semih <semihotun1@gmail.com>
2025-02-16 15:50:55 +0300

    Remove sln docket etc

--- docker-compose-service-UI.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Docker Compose başlatılıyor...
-docker-compose -f docker-compose-service-UI.yml up -d
-pause
--- docker-compose-service.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-echo Docker Compose başlatılıyor...
-docker-compose -f docker-compose-service.yml up -d
-pause
